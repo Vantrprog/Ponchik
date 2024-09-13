566515e4232aeb7fd84f2f90e5298c42fe0334ba
Vantrprog <vanyusha.filippov.07@mail.ru>
2024-09-13 11:29:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+let Godov=1
+let GD=365.25
+let sutka=30
+let hour=24
+let min=60
+let sec=60
+
+console.log(`№1.Количество секунд в сутках: ${hour*min*sec} секунд`)
+console.log(`№2.Количество секунд в 30 сутках: ${sutka *hour*min*sec} секунд`)
+console.log(`№3.Количество секунд в году: ${GD*hour*min*sec} секунд`)
+console.log(`№4.Количество минут в сутках: ${hour*min} минут`)
+console.log(`№5.Количество минут в году: ${GD*hour*min*sec} минут`)
+
+let Bait1=1024
+let mehabait=1024
+let hikabait=1024
+let kikobait=1024
+
+console.log(`№6.Байт в килобайте: ${Bait1*1} байт`)
+console.log(`№7.Байт в мегабайте: ${kikobait*Bait1} байт`)
+console.log(`№8.Байт в гигабайте: ${kikobait*Bait1*mehabait} байт`)
+console.log(`№9.Байт в терабайте: ${kikobait*Bait1*hikabait*mehabait} байт`)
+console.log(`№10.Килобайт в терабайте: ${kikobait*hikabait*mehabait} килобайт`)
